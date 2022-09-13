6fc9724a49f5bbfac2cadcac11737e8080db8517
Nick Bayern <jeldak@icloud.com>
2022-09-13 19:40:25 +0300

    fix html

---
+++ src/js/01-gallery.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+function createGalleryItemsCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    }).join('');
+}
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsCardsMarkup(galleryItems);
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function onGalleryClick(e) {
+    e.preventDefault();
+
+    const bigImgSrc = e.target.dataset.source;
+        
+    const instance = basicLightbox.create(`
+    <img src="${bigImgSrc}" width="800" height="600">
+    `);
+
+    instance.show();
+}
+
+gallery.addEventListener('click', onGalleryClick);
